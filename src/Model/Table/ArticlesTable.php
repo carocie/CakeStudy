7254aa511c2eddb9c0b602f3c0aad3ceceb3e5ff
carocie <carocie@carocie.com>
2018-05-16 11:20:10 +0900

    add function edited.

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -2,10 +2,17 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Utility\Text;
 
 class ArticlesTable extends Table{
     public function initialize(array $config){
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave($event, $entity, $options){
+        $sluggedTitle = Text::slug($entity->title);
+        $entity->slug = substr($sluggedTitle,0,191);
+        
     }
 }
 
